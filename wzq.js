9b83ab6a8bf27338b3816bbcb4fc5747299d8801
dengpeng_5202 <1006125584@qq.com>
2018-01-13 10:28:08 +0800

    wuziqi

--- wzq.js
+++ wzq.js
@@ -6,16 +6,85 @@ $(function () {
     let blank={};
     let btn=$('button');
     let ai;
+    let f1=0,f2=0;
+    let fs=$('.fs>strong');
+    let audio=new Audio('ChineseBG.mp3');
+    let KG=true;
+    audio.play();
     btn.click(function () {
         $(this).css({
            border:'1px solid red'
         });
-        if ($(this).text()=='人机'){
+        if ($(this).text()=='人机对战'){
             ai=true;
-            $(this).off();
-        }else if ($(this).text()=='人人'){
+        }else if ($(this).text()=='人人对战'){
             ai=false;
-            $(this).off();
+        }else if ($(this).text()=='重新开始'){
+            hei={};
+            bai={};
+            blank={};
+            $('.Bw').removeClass('block');
+            $('.Ww').removeClass('block');
+            btn.css({border:'1px solid transparent'})
+            $('span').removeClass('black white pieces').remove();
+            for (let i=0;i<15;i++){
+                $('<i>').appendTo(box);
+                $('<b>').appendTo(box);
+                for (let j=0;j<15;j++){
+                    $('<span>').attr('id',i+"_"+j).appendTo(box).addClass('pieces').data('pos',{x:i,y:j});
+                    blank[i+"_"+j]=true;
+                }
+            }
+            flag=true;
+            box.on('click','.pieces',function () {
+                if ($(this).hasClass('black')||$(this).hasClass('white')){
+                    return;
+                }
+                let data=$(this).data('pos');
+                if (flag){
+                    $(this).addClass('black');
+                    hei[data.x+"_"+data.y]=true;
+                    delete blank[data.x+"_"+data.y];
+                    if (isSuccess(data,hei)===5){
+                        f1++;
+                        box.off('click');
+                        $('.Bw').delay(1000).addClass('block');
+                        fs[0].innerText=f1;
+                    }
+                    if (ai){
+                        let pos= position ();
+                        $("#"+pos.x+"_"+pos.y).addClass('white');
+                        bai[pos.x+"_"+pos.y]=true;
+                        delete blank[pos.x+"_"+pos.y];
+                        if (isSuccess(pos,bai)===5){
+                            f2++;
+                            box.off('click');
+                            $('.Ww').delay(1000).addClass('block');
+                            fs[1].innerText=f2;
+                        }
+                        return;
+                    }
+                }else {
+                    $(this).addClass('white');
+                    bai[data.x+"_"+data.y]=true;
+                    if (isSuccess(data,bai)===5){
+                        f2++;
+                        box.off('click');
+                        $('.Ww').delay(1000).addClass('block');
+                        fs[1].innerText=f2;
+                    }
+                }
+                flag=!flag;
+
+            });
+
+        }else if ($(this).text()=='背景音乐'){
+            if(KG){
+                audio.pause();
+            }else {
+                audio.play();
+            }
+            KG=!KG;
         }
     });
 
@@ -38,7 +107,9 @@ $(function () {
             hei[data.x+"_"+data.y]=true;
             delete blank[data.x+"_"+data.y];
             if (isSuccess(data,hei)===5){
-                box.off();
+                f1++;
+                fs[0].innerText=f1;
+                box.off('click');
                 $('.Bw').delay(1000).addClass('block');
             }
             if (ai){
@@ -47,7 +118,9 @@ $(function () {
                 bai[pos.x+"_"+pos.y]=true;
                 delete blank[pos.x+"_"+pos.y];
                 if (isSuccess(pos,bai)===5){
-                    box.off();
+                    f2++;
+                    fs[1].innerText=f2;
+                    box.off('click');
                     $('.Ww').delay(1000).addClass('block');
                 }
                 return;
@@ -56,7 +129,9 @@ $(function () {
             $(this).addClass('white');
             bai[data.x+"_"+data.y]=true;
             if (isSuccess(data,bai)===5){
-                box.off();
+                f2++;
+                fs[1].innerText=f2;
+                box.off('click');
                 $('.Ww').delay(1000).addClass('block');
             }
         }
@@ -98,7 +173,6 @@ $(function () {
             zx++;
         }
 
-        console.log(row,col,zx,yx);
         return Math.max(row,col,zx,yx);
 
     }
